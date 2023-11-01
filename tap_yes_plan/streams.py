2fb19b9cc8653b222f302475761676a1632d884a
BernardWez <bernard.wezeman@quantile.nl>
2023-11-01 12:13:04 +0100

    Adjust typing

--- tap_yes_plan/streams.py
+++ tap_yes_plan/streams.py
@@ -438,7 +438,7 @@ class EventsCustomStream(YesPlanStream):
                 th.Property("deal_overigekostenimpresariaat", th.StringType),
                 th.Property("deal_notitiesoverigekostenimpresariaat", th.StringType),
                 th.Property("deal_maxverliesbijdrage", th.IntegerType),
-                th.Property("deal_suppletietotbovengrens", th.StringType),
+                th.Property("deal_suppletietotbovengrens", th.IntegerType),
                 th.Property("financial_discount", th.StringType),
                 th.Property("sales_verwachtbezetingspercentage", th.NumberType),
                 th.Property("financial_buma_stemra_sabam", th.StringType),
