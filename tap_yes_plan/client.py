8f4b528a8b2b0becb0a71de981ac60eaac94e49c
BernardWez <bernard.wezeman@quantile.nl>
2024-01-23 17:48:05 +0100

    Add custom Authenticator class and update authenticate_request method

--- tap_yes_plan/client.py
+++ tap_yes_plan/client.py
@@ -1,11 +1,58 @@
 """REST client handling, including YesPlanStream base class."""
 from typing import Iterable
-from urllib.parse import parse_qs, urlparse
+from urllib.parse import parse_qs, urlencode, urlsplit, urlunsplit, urlparse
 
 import requests
 from singer_sdk.authenticators import APIKeyAuthenticator
 from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
+
+def _add_parameters(initial_url: str, extra_parameters: dict) -> str:
+    """Add parameters to an URL and return the new URL.
+
+    Args:
+        initial_url: The URL to add parameters to.
+        extra_parameters: The parameters to add.
+
+    Returns:
+        The new URL with the parameters added.
+    """
+    scheme, netloc, path, query_string, fragment = urlsplit(initial_url)
+    query_params = parse_qs(query_string, keep_blank_values=True)
+    query_params.update(extra_parameters)
+
+    new_query_string = urlencode(query_params, doseq=True)
+
+    if 'valuesonly' in query_params and not query_params['valuesonly'][0]:
+        new_query_string = new_query_string.replace('valuesonly=', 'valuesonly')
+
+    return urlunsplit((scheme, netloc, path, new_query_string, fragment))
+
+# Create a custom Authenticator class that inherits from APIKeyAuthenticator
+class YesPlanAuthenticator(APIKeyAuthenticator):
+
+    # Update the authenticate_request method to add the api_key to the request params
+    def authenticate_request(
+        self,
+        request: requests.PreparedRequest,
+    ) -> requests.PreparedRequest:
+        """Authenticate a request.
+
+        Args:
+            request: A `request object`_.
+
+        Returns:
+            The authenticated request object.
+
+        .. _request object:
+            https://requests.readthedocs.io/en/latest/api/#requests.PreparedRequest
+        """
+        request.headers.update(self.auth_headers)
+
+        if request.url:
+            request.url = _add_parameters(request.url, self.auth_params)
+
+        return request
 
 
 class YesPlanStream(RESTStream):
@@ -22,7 +69,7 @@ class YesPlanStream(RESTStream):
     @property
     def authenticator(self) -> APIKeyAuthenticator:
         """Return a new authenticator object."""
-        return APIKeyAuthenticator.create_for_stream(
+        return YesPlanAuthenticator.create_for_stream(
             self,
             key="api_key",
             value=self.config.get("api_key"),
